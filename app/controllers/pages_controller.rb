c64bc6aad647eea3e1847a54ca959a92738e08ea
ANGUYEN <nguyen.arafath@gmail.com>
2020-11-15 14:33:00 +0100

    Added Dash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
 
 
   def home
     if current_user
       redirect_to cakes_path
     end
+  end
+
+  def dashboard
+    @cakes = current_user.cakes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :cakes
   devise_for :users
  root 'pages#home'
+
+ get 'dashboard' => 'pages#dashboard'
 end
